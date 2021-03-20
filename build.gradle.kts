db3996f55c1ae6dee5e86cca3c6fff3abd0a7ba0
Karl Tauber <karl@jformdesigner.com>
2021-03-20 01:34:57 +0100

    Gradle: publish snapshot to oss.sonatype.org

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ version = if( getBoolean( "release" ) ) releaseVersion else developmentVersion
 
 plugins {
 	`java-library`
+	`maven-publish`
+	signing
 }
 
 // check required Java version
@@ -83,4 +85,82 @@ tasks.jar {
 	into( "META-INF/versions/9" ) {
 		from( sourceSets["java9"].output )
 	}
+}
+
+
+publishing {
+	publications {
+		create<MavenPublication>( "maven" ) {
+			artifactId = "jide-oss"
+			groupId = "com.formdev"
+
+			from( components["java"] )
+
+			pom {
+				name.set( "JIDE Common Layer" )
+				description.set( "JIDE Common Layer (Professional Swing Components)" )
+				url.set( "https://github.com/JFormDesigner/jide-oss" )
+
+				licenses {
+					license {
+						name.set( "GPL with classpath exception" )
+						url.set( "http://www.gnu.org/licenses/gpl.txt" )
+					}
+					license {
+						name.set( "Free commercial license" )
+						url.set( "http://www.jidesoft.com/purchase/SLA.htm" )
+					}
+				}
+
+				developers {
+					developer {
+						id.set( "jidesoft" )
+						name.set( "jidesoft" )
+						email.set( "support@jidesoft.com" )
+					}
+				}
+
+				scm {
+					connection.set( "scm:git:git://github.com/JFormDesigner/jide-oss.git" )
+					url.set( "https://github.com/JFormDesigner/jide-oss" )
+				}
+			}
+		}
+	}
+
+	repositories {
+		maven {
+			name = "OSSRH"
+
+			val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+			val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+			url = uri( if( getBoolean( "release" ) ) releasesRepoUrl else snapshotsRepoUrl )
+
+			credentials {
+				// get from gradle.properties
+				val ossrhUsername: String? by project
+				val ossrhPassword: String? by project
+
+				username = System.getenv( "OSSRH_USERNAME" ) ?: ossrhUsername
+				password = System.getenv( "OSSRH_PASSWORD" ) ?: ossrhPassword
+			}
+		}
+	}
+}
+
+signing {
+	// get from gradle.properties
+	val signingKey: String? by project
+	val signingPassword: String? by project
+
+	val key = System.getenv( "SIGNING_KEY" ) ?: signingKey
+	val password = System.getenv( "SIGNING_PASSWORD" ) ?: signingPassword
+
+	useInMemoryPgpKeys( key, password )
+	sign( publishing.publications["maven"] )
+}
+
+// disable signing of snapshots
+tasks.withType<Sign>().configureEach {
+	onlyIf { getBoolean( "release" ) }
 }
